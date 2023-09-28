aa64571a9ccb89da184f9324a73ca93858f23436
adrianeka <adrian.eka9@gmail.com>
2023-09-29 03:10:15 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
+    <link rel="stylesheet" href="/fontawesome-free-6.4.2-web/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap">
+</head>
+<body>
+    <header class="pt-5">
+        @isset($header)
+            {{ $header }}
+        @endisset
+    </header>
+
+    <main class="container">
+        {{ $slot }}
+    </main>
+</body>
+</html>
